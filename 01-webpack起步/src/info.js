63312a78553e3e5e065db2b6cb9006e569af1e7d
wangjianjun <wangjianjun0730@126.com>
2020-07-18 18:33:33 +0800

    webpack.js

---
+++ 01-webpack起步/src/info.js
@@ -0,0 +1,9 @@
+//ES6语法导出
+export const add2 = function(num1,num2){
+    return num1 + num2;
+}
+
+export  const mul2 = function(num1,num2){
+    return num1 - num2;
+}
+
--- 01-webpack起步/src/main.js
+++ 01-webpack起步/src/main.js
@@ -1,4 +1,8 @@
-const {add, mul} = require('./util.js')
+const {add, mul} = require('./util.js') //commonJS
+import {add2,mul2} from './info.js'     //ES6
 
-console.log(add(10,20));
-console.log(mul(30,80));
+console.log('commonJS => sum = ' + add(10,20));
+console.log('commonJS => mul = ' + mul(30,80));
+
+console.log('commonJS => sum2 = ' + add2(80,20));
+console.log('commonJS => mul2 = ' + mul2(50,80));
