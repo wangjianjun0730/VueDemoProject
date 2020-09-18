03d29119d962066458058d49b75896152cb49448
wangjianjun <wangjianjun0730@126.com>
2020-09-18 17:22:31 +0800

    webpack更新

--- 02-webpack配置/src/main.js
+++ 02-webpack配置/src/main.js
@@ -5,7 +5,9 @@ console.log("height = " + height);
 console.log("age = " + age);
 console.log("name = " + name);
 
-require('./css/nomal.css');
-require('./less/special.less');
+// require('./css/nomal.css');
+// require('./less/special.less');
+import './css/nomal.css'
+import './less/special.less'
 
 document.write('<h2>你好，李银河!</h2>')
