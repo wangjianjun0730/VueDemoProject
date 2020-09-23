880e650c0650998bec266e57d5f51a8805f4623f
wangjianjun <wangjianjun0730@126.com>
2020-09-23 19:40:39 +0800

    添加vue

--- 02-webpack配置/src/main.js
+++ 02-webpack配置/src/main.js
@@ -1,5 +1,6 @@
-import {add,mul} from './mathFunction'
-import {height,age,name} from './info'
+// import {add,mul} from './mathFunction'
+// import {height,age,name} from './info'
+import Vue from 'vue'
 
 console.log("height = " + height);
 console.log("age = " + age);
@@ -9,5 +10,12 @@ console.log("name = " + name);
 // require('./less/special.less');
 import './css/nomal.css'
 import './less/special.less'
-
 document.write('<h2>你好，李银河!</h2>')
+
+//使用vue进行开发
+new Vue({
+    el:'#app',
+    data:{
+        massage:'hello webpack!'
+    }
+})
